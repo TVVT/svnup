8be32c2cb6d756553bf093a34d95d5e47fcc50db
wov <wovfeng@gmail.com>
2014-12-30 16:53:46 +0800

    fixed bug when svn is updateing

--- index.js
+++ index.js
@@ -21,16 +21,25 @@ app.get('/', function (req, res) {
 });
 
 app.get('/up',function(req,res){
+    var testcmd = 'pgrep -l svn';
     // 获取svn更新路径，如果没有更新路径则默认为全局 
     var cmd = 'cd ~/yhd_pc/svn/page/ && svn up';
-    exec(cmd, function(err, stdout, stderr) {
-        if (err) {
-            res.status(500).end(err.toString());
-        } else {
-            res.end(stdout);
-        }
-    });
 
+    exec(testcmd,function(err,stdout,stderr){
+        if(err) {
+            res.status(500).end(err.toString());
+        }else if(stdout){
+            res.status(500).end('其他同事更新svn中....去群里吼下吧！');
+        }else{
+            exec(cmd, function(err, stdout, stderr) {
+                if (err) {
+                    res.status(500).end(err.toString());
+                } else {
+                    res.end(stdout);
+                }
+            });
+        }
+    })
 
 });
 
